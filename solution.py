73b32d8cd4bbe1ea88d02a9393af4c153db4ea1b
Vaier <62292356+Vaier@users.noreply.github.com>
2023-04-13 12:34:36 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,19 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy import stats
-from scipy.stats import mannwhitneyu
+import scipy.stats as stats
 
-chat_id = 1251251937 # Ваш chat ID, не меняйте название переменной
+chat_id = 317456038 #Ваш chat ID, не меняйте название переменной
 
-def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    alpha = 0.09
-    
-    pval = stats.ttest_ind(x, y).pvalue
-    if pval < alpha:
-      res = True
-    else:
-      res = False    
-    return res 
+def solution(x, y) -> bool: #Одна или две выборке на входе, заполняется исходя из условия
+    #Измените код этой функции
+    #Это будет вашим решением
+    #Не меняйте название функции и её аргументы
+    return stats.ttest_ind(x, y)[1] < 0.09
