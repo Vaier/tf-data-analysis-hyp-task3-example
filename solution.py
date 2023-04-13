e4e3ae15132012ab7c1ae3612bf9a49f84897ad3
Vaier <62292356+Vaier@users.noreply.github.com>
2023-04-13 12:49:48 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import scipy.stats as stats
 
-chat_id = 317456038 #Ваш chat ID, не меняйте название переменной
+chat_id = 1251251937 #Ваш chat ID, не меняйте название переменной
 
 def solution(x, y) -> bool: #Одна или две выборке на входе, заполняется исходя из условия
     #Измените код этой функции
