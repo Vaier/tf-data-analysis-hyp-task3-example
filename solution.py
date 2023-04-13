880ba48356acbab098334db2fc00d79f103c6e61
Vaier <62292356+Vaier@users.noreply.github.com>
2023-04-13 13:26:47 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
-from scipy import stats
-
+from scipy.stats import mannwhitneyu
 
 chat_id = 1251251937 # Ваш chat ID, не меняйте название переменной
 
+SGN_LVL = 0.09
+
 def solution(x: np.array, y: np.array) -> bool:
-    alpha = 0.09
-    stat, p_value = stats.ttest_ind(x, y, alternative='two-sided')
-    return ( p_value < alpha)
+    pval = mannwhitneyu(x, y, alternative="less").pvalue
+    if pval < SGN_LVL:
+        return True
+    # true: выборки не равны
+    return False
