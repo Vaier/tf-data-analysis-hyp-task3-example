710d8c96627b0ae801c4cac81731f8643a0911d2
Vaier <62292356+Vaier@users.noreply.github.com>
2023-04-13 13:05:08 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
-import scipy.stats as stats
+from scipy import stats
 
-chat_id = 1251251937 #Ваш chat ID, не меняйте название переменной
 
-def solution(x, y) -> bool: #Одна или две выборке на входе, заполняется исходя из условия
-    #Измените код этой функции
-    #Это будет вашим решением
-    #Не меняйте название функции и её аргументы
-    return stats.ttest_ind(x, y)[1] < 0.09
+chat_id = 1251251937 # Ваш chat ID, не меняйте название переменной
+
+def solution(x: np.array, y: np.array) -> bool:
+    alpha = 0.09
+    stat, p_value = stats.ttest_ind(x, y, alternative='two-sided')
+    return ( p_value < alpha)
